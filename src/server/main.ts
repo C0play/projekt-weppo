2d460ad66f03c96035b34dcaa696bc45f6159b2e
Błażej <blazej.jakubowski3000@gmail.com>
2026-01-17 22:07:10 +0100

    fix

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,90 +1,84 @@
-import express from 'express';
-import type { Request, Response } from 'express';
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-import { Server, Socket } from 'socket.io';
+import express from "express";
+import type { Request, Response } from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server, Socket } from "socket.io";
 
+import { Game, Player } from "./game";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-import { Game, Player } from './game'
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-
 let nicks = new Set<string>();
 let games = new Map<string, Game>();
 let rooms = new Map<string, string>(); // nick -> room
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.sendFile(join(__dirname, '../../client', 'index.html'));
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(join(__dirname, "../../client", "index.html"));
 });
 
+io.on("connection", (socket: Socket) => {
+  // Nick creation
+  socket.on("create_nick", (nick: string) => {
+    console.log(socket.id + " new_nick: " + nick);
+    if (nicks.has(nick)) {
+      socket.emit("nick_status", { available: false, nick });
+    } else {
+      nicks.add(nick);
+      socket.emit("nick_status", { available: true, nick });
+    }
+  });
 
-io.on('connection', (socket: Socket) => {
+  socket.on("disconnect", (nick: string) => {
+    nicks.delete(nick);
+    rooms.delete(nick);
+  });
 
-    // Nick creation
-    socket.on('create_nick', (nick: string) => {
-        console.log(socket.id + ' new_nick: ' + nick)
-        if (nicks.has(nick)) {
-            socket.emit("nick_status", { available: false, nick });
-        } else {
-            nicks.add(nick);
-            socket.emit("nick_status", { available: true, nick });
-        }
+  // Get games
+  socket.on("get_games", () => {
+    let game_ids: Array<string> = new Array();
+    games.forEach((game, id) => {
+      if (game.number_of_players < game.max_players) {
+        game_ids.push(id);
+      }
     });
+    socket.emit("game_ids", game_ids);
+  });
 
-    socket.on('disconnect', (nick: string) => {
-        nicks.delete(nick);
-        rooms.delete(nick);
-    });
+  // Add game
+  socket.on("add_game", (nick: string) => {
+    let new_game: Game = new Game();
+    new_game.add_player(new Player(nick));
 
-    // Get games
-    socket.on('get_games', () => {
-        let game_ids: Array<string> = new Array();
-        games.forEach((game, id) => {
-            if (game.number_of_players < game.max_players) {
-                game_ids.push(id)
-            }
-        })
-        socket.emit("game_ids", game_ids)
-    })
+    games.set(new_game.uid, new_game);
+    rooms.set(nick, new_game.uid);
 
-    // Add game
-    socket.on('add_game', (nick: string) => {
-        let new_game: Game = new Game()
-        new_game.add_player(new Player(nick))
+    socket.join(new_game.uid);
+    socket.emit("game_added", new_game);
+  });
 
-        games.set(new_game.uid, new_game)
-        rooms.set(nick, new_game.uid)
+  // Enter game
+  socket.on("join_game", (nick: string, game_id: string) => {
+    const game = games.get(game_id);
+    if (game === undefined) {
+      socket.emit("error", { msg: "no game with " + game_id + " found" });
+      return;
+    }
+    game.add_player(new Player(nick));
+    rooms.set(nick, game.uid);
 
-        socket.join(new_game.uid)
-        socket.emit("game_added", new_game)
-    })
-    
-    // Enter game
-    socket.on('connect', (nick: string, game_id: string) => {
-        const game = games.get(game_id);
-        if (game === undefined) {
-            socket.emit("error", { msg: "no game with " + game_id + " found" })
-            return
-        }
-        game.add_player(new Player(nick))
-        rooms.set(nick, game.uid)
-        
-        socket.join(game.uid)
-        io.to(game.uid).emit("game", game)
-    })
+    socket.join(game.uid);
+    io.to(game.uid).emit("game", game);
+  });
 
-    // 
+  //
 });
-
 
 server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
+  console.log("server running at http://192.168.0.200:3000");
 });
