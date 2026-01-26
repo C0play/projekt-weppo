cbdb73e27b013fbc0a5bf5654af5bba3fb2c8522
Kamil <kpawlik2005@gmail.com>
2026-01-26 20:43:50 +0100

    added host to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src/client',
-  publicDir: '../../public',
+  root: "src/client",
+  publicDir: "../../public",
   build: {
-    outDir: '../../dist/client',
+    outDir: "../../dist/client",
     emptyOutDir: true,
   },
   server: {
+    host: "0.0.0.0",
     proxy: {
-      '/socket.io': {
-        target: 'http://localhost:3000',
-        ws: true
-      }
-    }
-  }
-})
+      "/socket.io": {
+        target: "http://localhost:3000",
+        ws: true,
+      },
+    },
+  },
+});
